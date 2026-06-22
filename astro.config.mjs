3e160b34423fc3862875e7aa283303c4ce4102f4
Ruben <rubenpi1996@gmail.com>
2026-06-22 09:12:44 +0200

    Subida produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,18 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+const NOINDEX_PATHS = [
+  '/aviso-legal/',
+  '/politica-de-privacidad/',
+  '/politica-de-cookies/',
+];
+
 export default defineConfig({
   site: 'https://easychoose.es',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) => !NOINDEX_PATHS.some((path) => page.endsWith(path)),
+    }),
+  ],
   trailingSlash: 'always',
 });
